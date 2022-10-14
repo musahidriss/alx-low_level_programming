8d33f467f916cbbfd3cac781696a27e9866fe5c0
musahidriss <musah.idriss@gmail.com>
2022-10-13 19:10:50 -0700

    commit 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 	printf("Size of a char : %i bytes(s)\n", sizeof(char));
-	printf("Size of a int : %i bytes(s)\n", sizeof(int));
+	printf("Size of an int : %i bytes(s)\n", sizeof(int));
 	printf("Size of a long int : %i bytes(s)\n", sizeof(long int));
 	printf("Size of a long long int : %i bytes(s)\n", sizeof(long long int));
 	printf("Size of a float : %i bytes(s)\n", sizeof(float));
