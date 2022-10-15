2402d4b0b895b50d63c67ecbbddd2eedf8196ca6
musahidriss <musah.idriss@gmail.com>
2022-10-14 19:03:13 -0700

    commit 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lower case
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+
+	char lc
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
