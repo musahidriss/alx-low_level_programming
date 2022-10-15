e328a09991e64c5fbb637320e1aea75b2fd23325
musahidriss <musah.idriss@gmail.com>
2022-10-14 19:36:22 -0700

    commit5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers 0 to 9
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d"\n, a);
+	}
+	return (0);
+}
