e3415673e5d7fb4f7a50c686727681ac0f77eb77
musahidriss <musah.idriss@gmail.com>
2022-10-14 20:13:11 -0700

    commit 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers 0 to 9 and alphabets 'a' to 'f'
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char al;
+	int num,
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+	for (al = 'a'; al <= 'f'; al++)
+	{
+		putchar(al);
+	}
+	putchar('\n');
+	return (0);
+}
