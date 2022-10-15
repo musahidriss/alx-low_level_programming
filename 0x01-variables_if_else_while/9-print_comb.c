3cd585aa0212bdbd70ede0ed03b071da96185f85
musahidriss <musah.idriss@gmail.com>
2022-10-14 20:33:32 -0700

    commit 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all combination of single digits
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar((num);
+				if (num != 57)
+				putchar(',');
+				putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
