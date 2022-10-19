af3bba54bbb9a80f7ed140418d8306ae50cca626
musahidriss <musah.idriss@gmail.com>
2022-10-18 18:40:14 -0700

    commit 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absoulte value of a number
+ * @t: the number to be computed
+ * Return: the absoulte value
+ */
+
+int _abs(int t)
+{
+	if (t > 0)
+		return (t);
+	else
+		return (-t);
+}
