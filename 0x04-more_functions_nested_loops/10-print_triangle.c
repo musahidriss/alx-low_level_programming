46c779a08ad17c0a0c9fe318747503e8a6693ea8
musahidriss <musah.idriss@gmail.com>
2022-10-21 21:29:11 -0700

    commit 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triange - prints a triangle
+ * @n: the size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int h, tr;
+
+	if (n > 0)
+	{
+		for (h = 1; h <= n, h++)
+		{
+			for (tr = n - h; tr > 0, tr--)
+				_putchar(' ');
+
+			for (tr = 0; tr < h, tr++)
+				_putchar('#');
+
+			if (h == n)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+}
