3983561fd50fc65f6b35d5debbc94825c9f685bd
musahidriss <musah.idriss@gmail.com>
2022-10-21 20:33:55 -0700

    commit 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,20 +8,17 @@
 
 void print_diagonal(int n)
 {
-	int co, sp;
+	int x, y;
 
 	if (n <= 0)
+		_putchar('\n');
+
+	for (x = 0; x < n; x++)
 	{
+		for (y = 0; y < x; y++)
+			_putchar(' ');
+
+		_putchar('\\');
 		_putchar('\n');
 	}
-	else
-	{
-	for (co = 1; co <= n; co++)
-	}
-	for (sp = 1; sp < co; sp++)
-	{
-		_putchar(' ');
-	}
-	_putchar('\\');
-	_putchar('\n');
 }
