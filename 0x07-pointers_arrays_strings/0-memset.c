028b576a77c5ec1c4b38fa31c6c6cd1d0c38a45e
musahidriss <musah.idriss@gmail.com>
2022-10-31 16:01:50 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,7 +13,7 @@ void *_memset(void *s, int c, size_t n)
 {
 	unsigned int index;
 	unsigned char *memory = s, value = c;
-	
+
 	for (index = 0; index < n; index++)
 		memory[index] = value;
 	return (memory);
