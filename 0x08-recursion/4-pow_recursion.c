73987b1837afa26c6de2135f63a61946269b6417
musahidriss <musah.idriss@gmail.com>
2022-11-03 05:36:03 -0700

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,14 +9,9 @@ include "main.h"
 
 int _pow_recursion(int x, int y)
 {
-	int result = x;
-
 	if (y < 0)
 		return (-1);
-	else if (y == 0)
+	if (y == 0)
 		return (1);
-
-	result *= _pow_recursion(x, y - 1);
-
-	return (result);
+	return (x * _pow_recursion(x, y - 1));
 }
