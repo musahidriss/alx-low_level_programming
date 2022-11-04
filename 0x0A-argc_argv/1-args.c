d18286da0cb944a22ac5c70a8a7ad6091da822f8
musahidriss <musah.idriss@gmail.com>
2022-11-04 16:01:07 -0700

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,15 +10,10 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+	(void) argv;
 
-	if (argc == 1)
-		printf("%d\n", argc - 1);
-	else
-	{
-		for (i = 0; *argv; i++, argv++)
-			;
-		printf("%d\n", i - 1)
-	}
+	/*Ignore argv*/
+	printf("%i\n", argc - 1);
+
 	return (0);
 }
