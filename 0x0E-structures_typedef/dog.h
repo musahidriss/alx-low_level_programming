07a5516a648f3b92b112b4917f2c9a93c7e1f9c2
musahidriss <musah.idriss@gmail.com>
2022-11-14 14:52:24 -0800

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,12 @@
-#ifndef "DOG_H"
-#define "DOG_H"
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - A simple struct that contains variables
  * @name: Holds name of dog in char array
  * @age: Holds number, including any decimals
  * @owner: Holds name of owner in char array
+ * Description: this is a struct for a dog
  */
 
 struct dog
@@ -14,10 +15,6 @@ struct dog
 	float age;
 	char *owner;
 };
-
-/**
- * dog_t - Typedef for dog structure
- */
 
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
